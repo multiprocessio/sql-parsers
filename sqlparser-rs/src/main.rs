4d4e0bdc7f06856e8537d1e2d7bbaff41d396d36
Phil Eaton <phil@eatonphil.com>
2022-04-12 01:01:55 +0000

    Cargo fmt

--- sqlparser-rs/src/main.rs
+++ sqlparser-rs/src/main.rs
@@ -3,21 +3,21 @@ use sqlparser::parser::Parser;
 
 fn main() {
     let simple = "SELECT * FROM x";
-    
+
     let medium = "SELECT COUNT(1) AS count, name section FROM t GROUP BY name LIMIT 10";
-    
+
     let complex = "
 SELECT 
-	country.country_name_eng,
-	SUM(CASE WHEN talk.id IS NOT NULL THEN 1 ELSE 0 END) AS talks,
-	AVG(ISNULL(DATEDIFF(SECOND, talk.start_time, talk.end_time),0)) AS avg_difference
+        country.country_name_eng,
+        SUM(CASE WHEN talk.id IS NOT NULL THEN 1 ELSE 0 END) AS talks,
+        AVG(ISNULL(DATEDIFF(SECOND, talk.start_time, talk.end_time),0)) AS avg_difference
 FROM country 
 LEFT JOIN city ON city.country_id = country.id
 LEFT JOIN customer ON city.id = customer.city_id
 LEFT JOIN talk ON talk.customer_id = customer.id
 GROUP BY 
-	country.id,
-	country.country_name_eng
+        country.id,
+        country.country_name_eng
 HAVING AVG(ISNULL(DATEDIFF(SECOND, talk.start_time, talk.end_time),0)) > (SELECT AVG(DATEDIFF(SECOND, talk.start_time, talk.end_time)) FROM talk)
 ORDER BY talks DESC, country.id ASC;
 ";
@@ -28,9 +28,9 @@ ORDER BY talks DESC, country.id ASC;
 
     let dialect = GenericDialect {};
     for test in tests {
-	match Parser::parse_sql(&dialect, test) {
-	    Ok(ast) => println!("AST: {:#?}", ast),
-	    Err(err) => println!("Error: {:#?}", err),
-	}
+        match Parser::parse_sql(&dialect, test) {
+            Ok(ast) => println!("AST: {:#?}", ast),
+            Err(err) => println!("Error: {:#?}", err),
+        }
     }
 }
